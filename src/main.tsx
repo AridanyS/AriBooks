7d10ef9fff56c3f7933d04d7b00f26ff36555af3
Aridany <aridany54@gmail.com>
2024-06-05 23:42:06 +0200

    Check components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import BooksGrid from './components/books.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <BooksGrid />
   </React.StrictMode>,
 )
